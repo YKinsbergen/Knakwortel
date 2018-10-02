ea774d3f0c2c01b2541421614f70bf42fb9809bb
Peter van Dijk <petervandijk@gmail.com>
2018-10-02 16:16:47 +0200

    display api fetched data

--- website/routes/home.js
+++ website/routes/home.js
@@ -1,8 +1,14 @@
 const router = require('koa-router')()
 const parseBody = require('koa-body')
+const axios = require('axios')
 
 router.get('/', async (ctx, next) => {
-  ctx.render('index', { title: 'Home' })
+  const starwarsPeople = await axios(`https://swapi.co/api/people/`)
+  ctx.render('index', 
+    { 
+      title: 'Home',
+      people: starwarsPeople.data
+    })
   await next()
 })
 
