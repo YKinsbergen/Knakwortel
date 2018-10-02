e21c7ca0d2318f70c6def77c36fe58fa7d57de14
YKinsbergen <Yannick.kinsbergen@hotmail.com>
2018-10-02 10:19:42 +0200

    Added boilerplate client and server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom' 
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Knakwortel</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
