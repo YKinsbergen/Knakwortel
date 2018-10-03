988d3b6e6c6d5aeca5c6a73ce405be88cc752e58
Peter van Dijk <petervandijk@gmail.com>
2018-10-03 16:25:13 +0200

    changed endpoint for pageContent order

--- server/src/pages/controller.ts
+++ server/src/pages/controller.ts
@@ -1,6 +1,6 @@
 // src/advertisements/controller.ts
 import { JsonController, Get, Param} from "routing-controllers";
-import {Page} from './entities'
+import {Page, PageContent} from './entities'
 
 @JsonController()
 export class PagesController {
@@ -8,7 +8,11 @@ export class PagesController {
     async onePage (
         @Param('id') id: number
     ) {
-        const page = await Page.findOne(id, {relations: ['pageTitle', 'pageContents', 'pageContents.image']})
+        const page = await Page.findOne(id, {relations: ['pageTitle']})
+        const pageContents = await PageContent.find({where: {page}, order: {order: 'ASC'}})
+
+
+        if (page) page.pageContents = pageContents
         return page
     }
 }
