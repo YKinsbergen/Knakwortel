dc55820ba5a38217aab0c77284bf895a51b55ff3
Peter van Dijk <petervandijk@gmail.com>
2018-10-04 17:05:24 +0200

    got it working\

--- website/routes/home.js
+++ website/routes/home.js
@@ -7,13 +7,26 @@ const apiUrl = constants.apiUrl
 router.get('/', async (ctx, next) => {
   const pageId = 1
   const pageRequest = await axios(`${apiUrl}/pages/${pageId}`)
+  
+  const instaMediaArray = await instagramPhotos()
+
   const page = pageRequest.data
   ctx.render('index', 
     { 
       title: page.pageTitle.content,
-      page: page
+      page: page,
+      instaFeed: instaMediaArray
     })
   await next()
 })
+
+async function instagramPhotos() {
+  const userPageSource = await axios.get('https://www.instagram.com/knakwortel/')
+  const jsonObject = userPageSource.data.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1)
+
+  const userInfo = JSON.parse(jsonObject)
+  const mediaArray = userInfo.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges
+  return mediaArray
+}
 
 module.exports = router
