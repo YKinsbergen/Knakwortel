5a3260ddf2f6dfe5fcf4a1f827f33873e1fb68dc
gitstephanie <s.dejong@mail.com>
2018-10-04 15:47:41 +0200

    Added SEO-endpoints and entities, stillneed to add Google Analytics

--- server/src/seo/controller.ts
+++ server/src/seo/controller.ts
@@ -1,29 +1,37 @@
 // src/advertisements/controller.ts
-import { JsonController, Get } from "routing-controllers";
+import { JsonController, Get, Put, Param, Body, BadRequestError, NotFoundError, HttpCode } from "routing-controllers";
 import SeoTag from './entities'
 
 @JsonController()
 export class SeoController {
     @Get('/seotag')
     allSeoTags = async() => {
-        const seoTags = await SeoTag.find()
+        const seoTags = await SeoTag.find({relations: ['seoTagAttributes']})
         return {seoTags}
     }
 
-    // @Get('/ads/:id')
-    // getAd(
-    // @Param('id') id: number) { 
-    //     return Placeholder.findOne(id)
-    // }
+    @Get('/seotag/:id')
+    async oneSeoTag(
+        @Param('id') id: number
+    ) {
+        const seoTag = await SeoTag.findOne(id, {relations: ['seoTagAttributes']})
+        if (!seoTag) throw new BadRequestError('That pageContent does not exist')
+        return seoTag
+    }
+    
+    //@Authorized
+    @HttpCode(201)
+    @Put('/contents/:id([0-9]+)')
+    async updatePageContent(
+        @Param('id') id: number,
+        @Body() update: Partial<SeoTag>
+    ) {
+        const tag = await SeoTag.findOne(id)
+        if (!tag) throw new NotFoundError('Cannot find SEO-tag')
+    
+        return SeoTag.merge(tag, update).save()
+    }
+}
 
-    // @Post('/ads')
-    // @HttpCode(201)
-    // createAd(
-    //   @Body() placeholder: Placeholder
-    // ) {
-    //   return placeholder.save()
-    // }
-
-
-    // Link met Google Analytics??
+   // Link met Google Analytics?? 
 }
