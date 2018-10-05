3efe3c87a90f9c905f6ea0929133910df49e8466
Peter van Dijk <petervandijk@gmail.com>
2018-10-05 20:54:02 +0200

    shops, csv file upload, all shops stored in db

---
+++ admin/src/actions/shops.js
@@ -0,0 +1,39 @@
+import request from 'superagent'
+import {apiUrl} from '../constants'
+
+export const SHOPS_FETCHED = 'SHOPS_FETCHED'
+export const NEW_SHOPS_FETCHED = 'NEW_SHOPS_FETCHED'
+
+function shopsFetched(shops) {
+  return {
+    type: SHOPS_FETCHED,
+    payload: shops
+  }
+}
+
+function newShopsFetched(shops) {
+  return {
+    type: NEW_SHOPS_FETCHED,
+    payload: shops
+  }
+}
+
+export const addShops = (shopsArrOfObj) => (dispatch, getState) => {
+  // const state = getState()
+  // const jwt = state.currentUser.jwt
+ 
+  // if (isExpired(jwt)) return dispatch(logout())
+
+  request
+    .post(`${apiUrl}/shops`)
+    // .set('Authorization', `Bearer ${jwt}`)
+    .send(shopsArrOfObj)
+    .then(response => dispatch(newShopsFetched(response.body.newShops)))
+    .catch(console.error)
+}
+
+export const getShops = () => (dispatch) => {
+  request(`${apiUrl}/shops`)
+    .then(response => dispatch(shopsFetched(response.body)))
+    .catch(err => console.error(err))
+}
---
+++ admin/src/components/Shops.js
@@ -0,0 +1,49 @@
+
+import React from 'react'
+import './Dashboard.css'
+import { Link } from 'react-router-dom'
+
+
+
+export default function Shops(props) {
+  return (
+    <div>
+      <div class="pt-3 pb-2 mb-3">
+        <h2>Shops (Verkooppunten)</h2>
+        <input type="file" name="shopsCsv" id="shopsCsv" onChange={props.addShopsOnChange}/>
+          <p className="text-muted"><small>Voeg batch van shops toe via CSV file (format: naam,adres,1234XX,latitude,longitude)</small></p>
+
+      </div>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Naam</th>
+                  <th>Adres</th>
+                  <th>Postcode</th>
+                  <th>Latitude</th>
+                  <th>Longitude</th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.shops.map(shop => 
+                <tr key={shop.id}>
+                  <td>link..</td>
+                  <td>{shop.name}</td>
+                  
+                  <td>{shop.address}</td>
+
+                  <td>{shop.zipcode}</td>
+
+                  <td>{shop.latitude}</td>
+                  <td>{shop.longitude}</td>
+                </tr>  
+                )}
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+  )
+}
