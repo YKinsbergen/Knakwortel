4469587909f4d3dd1414fd4e8838451a975af7d1
gitstephanie <s.dejong@mail.com>
2018-10-08 16:08:46 +0200

    Fixed delete endpoint, everything works fine now.

--- server/src/sweaterorders/controller.ts
+++ server/src/sweaterorders/controller.ts
@@ -1,19 +1,19 @@
-import { JsonController, Post, Param, Get, Body, Put, NotFoundError, HttpCode, Delete, Authorized } from 'routing-controllers'
+import { JsonController, Post, Param, Get, Body, Put, NotFoundError, HttpCode, Delete, Authorized} from 'routing-controllers'
 import Order from './entity';
 
 @JsonController()
 export default class OrderController {
 
-  @Post('/order')
+  @Post('/orders')
   @HttpCode(201)
   async createOrder(
-    @Body() order: Order,
+    @Body() order: Partial<Order>,
   ) {
         await Order.create(order).save()
         return order
   }
 
-  @Put('/order/:id([0-9]+)')
+  @Put('/orders/:id([0-9]+)')
   async updateOrder(
      @Body() update: Partial<Order>,
      @Param('id') id: number 
@@ -24,27 +24,27 @@ export default class OrderController {
   }
 
   @Authorized()
-  @Get('/order/:id([0-9]+)')
+  @Get('/orders/:id([0-9]+)')
   getOrder(
     @Param('id') id: number
   ) {
     return Order.findOne(id)
   }
 
-  @Authorized()
   @Get('/orders')
   allOrders = async() => {
     const orders = await Order.find()
     return orders
   }
 
-  @Delete('orders/:id([0-9]+)')
+  @Delete('/orders/:id')
    async deleteOrder(
-       @Param('id') id: number,
+        @Param('id') id: number,
    ) {
-       const orderToDelete = await Order.findOne(id)
-       if (!orderToDelete) throw new NotFoundError ('Cannot find order')
-       return Order.delete(orderToDelete)
+        const orderToDelete = await Order.findOne(id)
+        if (!orderToDelete) throw new NotFoundError ('Cannot find order')
+        return Order.delete(orderToDelete)
+      
    }
 
 }
